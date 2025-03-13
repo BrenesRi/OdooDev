9c0da15f28d8d1dd60a0ade7259d4aa495a4756b
BrenesRi <ribrenes10@gmail.com>
2025-03-12 19:15:14 -0600

    Chapter 6: Complete

--- realestate/models/estate_property.py
+++ realestate/models/estate_property.py
@@ -1,25 +1,38 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from datetime import date
+from dateutil.relativedelta import relativedelta
+from odoo import api, fields, models
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
-    name = fields.Char('Plan Name', required=True, translate=True)
-    description = fields.Text('Description')
-    postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Availability Date')
-    expected_price = fields.Float('Expected Price', requiered=True)
-    selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('Number of Bedrooms')
-    living_area = fields.Integer('Living Area (m²)')
-    facades = fields.Integer('Number of Facades')
-    garage = fields.Boolean('Garage')
-    garden = fields.Boolean('Garden')
+    name = fields.Char(string="Plan Name", required=True, translate=True)
+    description = fields.Text(string='Description')
+    postcode = fields.Char(string='Postcode')
+    date_availability = fields.Date(string="Available From", copy=False, default=date.today() + relativedelta(months=+3))
+    expected_price = fields.Float(string='Expected Price', requiered=True)
+    selling_price = fields.Float(string='Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer(string='Living Area (m²)')
+    facades = fields.Integer(string='Number of Facades')
+    garage = fields.Boolean(string='Garage')
+    garden = fields.Boolean(string='Garden')
     garden_area = fields.Integer('Garden Area (m²)')
-    garden_orientation = fields.Selection(
-        [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
-        'Garden Orientation'
+    garden_orientation = fields.Selection(selection=[
+                                        ('north', 'North'), 
+                                        ('south', 'South'), 
+                                        ('east', 'East'), 
+                                        ('west', 'West')],
+                                        string='Garden Orientation'
     )
+    active = fields.Boolean(string='Active', default=True)
+    state = fields.Selection(selection=[('New',"New"),
+                                      ('Offer Received',"Offer Received"),
+                                      ('Offer Accepted',"Offer Accepted"),
+                                      ('Sold',"Sold"),
+                                      ('Canceled',"Canceled")], 
+                                      default='New', string='State', required=True, copy=False)
+ 
