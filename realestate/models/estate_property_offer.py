f005a4b58ffce84616aef1213702c6406923b397
BrenesRi <ribrenes10@gmail.com>
2025-03-17 21:17:04 -0600

    Chapter 9: Compute

--- realestate/models/estate_property_offer.py
+++ realestate/models/estate_property_offer.py
@@ -14,3 +14,36 @@ class PropertyOffer(models.Model):
     # Relation
     partner_id = fields.Many2one('res.partner', string="Partner", required=True)
     property_id = fields.Many2one('estate.property', required=True)
+
+    # Compute
+    validity = fields.Integer('Validity (days)', default=7)
+    date_deadline = fields.Date('Deadline', compute='_get_deadline', inverse='_set_deadline')
+
+    @api.depends('create_date','validity')
+    def _get_deadline(self):
+        """
+        The function calculates the deadline by adding the validity duration to the create date.
+        """
+        
+        for record in self:
+            if not (record.create_date and record.validity):
+                start = datetime.now()
+                duration = timedelta(record.validity, seconds=-1)
+                record.date_deadline = start + duration
+                continue
+            
+            start = fields.Datetime.from_string(record.create_date)
+            duration = timedelta(record.validity, seconds=-1)
+            record.date_deadline = start + duration
+    
+    def _set_deadline(self):
+        for record in self:
+            if not (record.create_date and record.validity):
+                start = datetime.now()
+                duration = timedelta(record.validity, seconds=-1)
+                record.date_deadline = start + duration
+                continue
+
+            start = fields.Datetime.from_string(record.create_date)
+            end = fields.Datetime.from_string(record.date_deadline)
+            record.validity = (end - start).days + 1
